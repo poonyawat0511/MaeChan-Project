0c5a6f25d0359c35b96e21aa22deed051fe5a729
poonyawat0511 <poonyawat0931@gmail.com>
2025-01-16 23:35:42 +0700

    backend : new path find purcahse by camunda task id

--- backend/notification/src/main/java/com/example/workflow/repository/PurchaseRequestRepository.java
+++ backend/notification/src/main/java/com/example/workflow/repository/PurchaseRequestRepository.java
@@ -1,5 +1,7 @@
 package com.example.workflow.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +18,9 @@ public interface PurchaseRequestRepository extends JpaRepository<PurchaseRequest
     @Query("UPDATE PurchaseRequest pr SET pr.camundaTaskId = :taskId WHERE pr.id = :purchaseId")
     void updateCamundaTaskId(@Param("purchaseId") Long purchaseId, @Param("taskId") String taskId);
 
+
+    @Query("SELECT pr FROM PurchaseRequest pr WHERE pr.camundaTaskId = :taskId")
+    Optional<PurchaseRequest> findPurchaseRequestByCamundaTaskId(@Param("taskId") String taskId);
+    
+    
 }
