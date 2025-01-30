14280d36802cba697eee07921187b2ba7dc6495b
Osathi Jantrasri <otfor01@gmail.com>
2025-01-30 12:12:55 +0700

    test

--- backend/notification/src/main/java/com/example/workflow/component/ScheduledTest2.java
+++ backend/notification/src/main/java/com/example/workflow/component/ScheduledTest2.java
@@ -9,7 +9,7 @@ public class ScheduledTest2 {
     // MON-FRI                  จันทร์-ศุกร์
     // "0 59 23 * * MON-FRI"   5 ทุ่ม 59 นาที 
     // "0 5 0 * * MON-FRI"     0 นาฬิกา 5 นาที
-    @Scheduled(cron = "0 5 0 * * MON-FRI", zone = "Asia/Bangkok") 
+    @Scheduled(cron = "0 7 12 * * MON-FRI", zone = "Asia/Bangkok") 
     public void testMessage() { 
         System.out.println("This is your scheduled message! Test2");
     }
