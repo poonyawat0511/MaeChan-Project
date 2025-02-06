2051dd2cf0c57ffb9643bc21f8ca520915ff6aaa
Nattawat Nattachanasit <6531503201@lamduan.mfu.ac.th>
2025-02-06 17:46:17 +0700

    feat: add .gitignore, enhance sign-in page styling, and implement responsive layout

--- frontend/processes/src/app/signin/page.tsx
+++ frontend/processes/src/app/signin/page.tsx
@@ -1,7 +1,11 @@
 "use client";
+import Image from 'next/image'
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { SignInResponse } from "@/utils/types/signInResponse";
+import styles from './styles.module.css'
+import logo from '../../../public/logo66.png'
+
 import axios from "axios";
 
 const SignInPage = () => {
@@ -35,46 +39,51 @@ const SignInPage = () => {
   };
 
   return (
-    <div className="flex h-full justify-center items-center">
-      <form
-        className="bg-white p-8 rounded-lg shadow-md max-w-sm w-full"
-        onSubmit={handleSignIn}
-      >
-        <h1 className="text-2xl font-bold mb-4 text-center">Sign In</h1>
-
-        {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm mb-2">Email</label>
-          <input
-            type="email"
-            className="w-full p-2 border rounded-md"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm mb-2">Password</label>
-          <input
-            type="password"
-            className="w-full p-2 border rounded-md"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded-md w-full"
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <form
+          className={styles.form}
+          onSubmit={handleSignIn}
         >
-          Sign In
-        </button>
-      </form>
+          <Image src={logo} alt="Logo" width={200} height={100} className='w-full'/>
+          
+          <h1 className="text-2xl font-bold text-left mb-4">Sign In</h1>
+
+          {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+
+          <div className="mb-4">
+            <label className={styles.input_label}>Email</label>
+            <input
+              type="email"
+              className={styles.textfield}
+              placeholder="example@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className={styles.input_label}>Password</label>
+            <input
+              type="password"
+              className={styles.textfield}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-[#003465] text-white py-2 px-4 rounded-md w-full"
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+
     </div>
   );
 };
