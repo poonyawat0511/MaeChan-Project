17fe290a649bd976cec93b47aa47ae04cfee3a2b
Nattawat Nattachanasit <6531503201@lamduan.mfu.ac.th>
2025-02-06 23:54:17 +0700

    feat: add SignInCard and Loader components, update styles, and configure HeroUI provider

--- frontend/processes/src/app/signin/page.tsx
+++ frontend/processes/src/app/signin/page.tsx
@@ -1,42 +1,43 @@
 "use client";
 import Image from 'next/image'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { SignInResponse } from "@/utils/types/signInResponse";
-import styles from './styles.module.css'
-import logo from '../../../public/logo66.png'
-
-import axios from "axios";
+import styles from './styles.module.css';
+import logo from '../../../public/logo66.png';
+import { Input } from '@heroui/input';
+import { EnvelopeIcon, EyeIcon, EyeSlashIcon, KeyIcon } from '@heroicons/react/24/outline';
+import React from 'react';
+import { Button } from '@heroui/button';
 
 const SignInPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
+  const [isVisible, setIsVisible] = React.useState(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
 
   const handleSignIn = async (e: React.FormEvent) => {
     e.preventDefault();
     setError("");
 
     try {
-      await axios.post<SignInResponse>(
-        "http://localhost:8081/auth/signin",
-        { email, password },
-        {
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
-        }
-      );
+      const data: SignInResponse = await fetch("http://localhost:8081/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+        credentials: "include",
+      },);
 
       router.push("/");
     } catch (err) {
-      if (axios.isAxiosError(err)) {
-        setError(err.response?.data?.message || "An error occurred");
-      } else {
-        setError("An unexpected error occurred");
-      }
+      setError(err.response.data.message);
     }
   };
+
+
+
 
   return (
     <div className={styles.container}>
@@ -45,42 +46,60 @@ const SignInPage = () => {
           className={styles.form}
           onSubmit={handleSignIn}
         >
-          <Image src={logo} alt="Logo" width={200} height={100} className='w-full'/>
-          
+          <div className='flex w-full items-center justify-center'>
+            <Image src={logo} alt="Logo" height={100} className='flex items-center' />
+          </div>
+
           <h1 className="text-2xl font-bold text-left mb-4">Sign In</h1>
 
           {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-
-          <div className="mb-4">
-            <label className={styles.input_label}>Email</label>
-            <input
-              type="email"
-              className={styles.textfield}
-              placeholder="example@email.com"
-              value={email}
+          <div className="space-y-8">
+            <Input
+              label="Email"
+              labelPlacement="outside"
+              placeholder="you@example.com"
+              startContent={
+                <EnvelopeIcon className="icon" />
+              }
               onChange={(e) => setEmail(e.target.value)}
-              required
+              type="email"
             />
-          </div>
 
-          <div className="mb-4">
-            <label className={styles.input_label}>Password</label>
-            <input
-              type="password"
-              className={styles.textfield}
-              placeholder="Password"
+            <Input
+              type={isVisible ? "text" : "password"}
+              labelPlacement='outside'
+              label="Password"
+              placeholder="********"
+
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              startContent={
+                <KeyIcon className="icon" />
+              }
+              endContent={
+                <button
+                  aria-label="toggle password visibility"
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={toggleVisibility}
+                >
+                  {isVisible ? (
+                    <EyeSlashIcon className="icon" />
+                  ) : (
+                    <EyeIcon className="icon" />
+                  )}
+                </button>
+              }
               required
             />
           </div>
 
-          <button
+          <Button
             type="submit"
-            className="bg-[#003465] text-white py-2 px-4 rounded-md w-full"
+            className="bg-[#003465] text-white w-full"
           >
             Sign In
-          </button>
+          </Button>
         </form>
       </div>
 
