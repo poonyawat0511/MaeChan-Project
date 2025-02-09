bddf135a137599f0f9ab66f45e46a23b8e1861ed
Osathi Jantrasri <otfor01@gmail.com>
2025-02-10 00:49:56 +0700

    delete StockUserApprove

--- backend/notification/src/main/java/com/example/workflow/repository/StockUserApproveRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.workflow.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.workflow.model.StockUserApprove;
-
-public interface StockUserApproveRepository extends JpaRepository<StockUserApprove, Long> {
-}
