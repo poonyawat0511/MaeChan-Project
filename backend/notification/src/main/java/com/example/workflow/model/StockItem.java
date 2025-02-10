9a1ffd3c183e618494e0a1724a5f7005b551c8e4
Osathi Jantrasri <otfor01@gmail.com>
2025-02-10 16:41:16 +0700

    update StockItem to have all column

--- backend/notification/src/main/java/com/example/workflow/model/StockItem.java
+++ backend/notification/src/main/java/com/example/workflow/model/StockItem.java
@@ -147,4 +147,141 @@ public class StockItem {
 
     @Column(name = "drugitems_no_substock")
     private boolean drugitemsNoSubstock;
+
+    //blank column below
+
+    @Column(name = "item_standard_price")
+    private double itemStandardPrice;
+
+    @Column(name = "item_unit_qty")
+    private int itemUnitQty;
+
+    @Column(name = "item_package_name")
+    private String itemPackageName;
+
+    @Column(name = "icode")
+    private String icode;
+
+    @Column(name = "standard_code")
+    private String standardCode;
+
+    @Column(name = "dummy_left_qty")
+    private int dummyLeftQty;
+
+    @Column(name = "dummy_left_price")
+    private double dummyLeftPrice;
+
+    @Column(name = "item_sub_unit_qty")
+    private int itemSubUnitQty;
+
+    @Column(name = "old_unit_cost")
+    private double oldUnitCost;
+
+    @Column(name = "newcode")
+    private String newcode;
+
+    @Column(name = "default_department_id")
+    private Long defaultDepartmentId;
+
+    @Column(name = "barcode_number")
+    private String barcodeNumber;
+
+    @Column(name = "hos_guid")
+    private String hosGuid;
+
+    @Column(name = "gpo_code")
+    private String gpoCode;
+
+    @Column(name = "didstd_code")
+    private String didstdCode;
+
+    @Column(name = "avg_month_use_qty")
+    private int avgMonthUseQty;
+
+    @Column(name = "item_regno")
+    private String itemRegno;
+
+    @Column(name = "bdg_nextyear_percent")
+    private double bdgNextyearPercent;
+
+    @Column(name = "item_note")
+    private String itemNote;
+
+    @Column(name = "avg_lead_day")
+    private int avgLeadDay;
+
+    @Column(name = "gpo_vmi")
+    private boolean gpoVmi;
+
+    @Column(name = "balance_qty")
+    private int balanceQty;
+
+    @Column(name = "item_eng")
+    private String itemEng;
+
+    @Column(name = "item_trand")
+    private String itemTrand;
+
+    @Column(name = "item_type_group")
+    private String itemTypeGroup;
+
+    @Column(name = "item_status_control")
+    private String itemStatusControl;
+
+    @Column(name = "item_van_type")
+    private String itemVanType;
+
+    @Column(name = "search_keyword")
+    private String searchKeyword;
+
+    @Column(name = "abc")
+    private String abc;
+
+    @Column(name = "ved_code")
+    private String vedCode;
+
+    @Column(name = "pharmacology_group1")
+    private String pharmacologyGroup1;
+
+    @Column(name = "pharmacology_group2")
+    private String pharmacologyGroup2;
+
+    @Column(name = "pharmacology_group3")
+    private String pharmacologyGroup3;
+
+    @Column(name = "vat_percent")
+    private double vatPercent;
+
+    @Column(name = "sap_unit_name")
+    private String sapUnitName;
+
+    @Column(name = "sap_item_name")
+    private String sapItemName;
+
+    @Column(name = "sap_unit_cost")
+    private double sapUnitCost;
+
+    @Column(name = "sap_active")
+    private boolean sapActive;
+
+    @Column(name = "manufacturer_list_text")
+    private String manufacturerListText;
+
+    @Column(name = "use_fixed_avg_cost")
+    private boolean useFixedAvgCost;
+
+    @Column(name = "fixed_avg_cost")
+    private double fixedAvgCost;
+
+    @Column(name = "stock_mrp_order_type_id")
+    private Long stockMrpOrderTypeId;
+
+    @Column(name = "stock_mrp_lot_size")
+    private int stockMrpLotSize;
+
+    @Column(name = "apply_vat")
+    private boolean applyVat;
+
+    @Column(name = "gpsc_code")
+    private String gpscCode;
 }
