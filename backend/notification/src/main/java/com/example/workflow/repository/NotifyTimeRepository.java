2fe8f1ce2dc35c00b09f4bc60f6088e6d8169159
Osathi Jantrasri <otfor01@gmail.com>
2025-02-15 13:24:22 +0700

    Notify finish simple

--- backend/notification/src/main/java/com/example/workflow/repository/NotifyTimeRepository.java
+++ backend/notification/src/main/java/com/example/workflow/repository/NotifyTimeRepository.java
@@ -4,6 +4,9 @@ import com.example.workflow.model.NotifyTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalTime;
+
 @Repository
 public interface NotifyTimeRepository extends JpaRepository<NotifyTime, Long> {
+    NotifyTime findByTime(LocalTime time);
 }
