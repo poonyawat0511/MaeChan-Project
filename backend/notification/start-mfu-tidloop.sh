1a4a364d3ada926db2df56a093c292481e564279
Osathi Jantrasri <otfor01@gmail.com>
2025-03-10 14:39:56 +0700

    feat: modified start script

--- backend/notification/start-mfu-tidloop.sh
+++ backend/notification/start-mfu-tidloop.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Get the IP address from the specified interface
 export IPADDRESS=$(ipconfig getifaddr en0)
-echo "Using IPADDRESS: $IPADDRESS"
-echo "Using IPADDRESS: $IPADDRESS"
+echo "mfu-tidloop Using Host IPADDRESS: $IPADDRESS"
 # Now run docker-compose with the exported IPADDRESS variable
 docker-compose up -d
 # --build
 
 # Make sure to give execute permissions to the script:
-# chmod +x start.sh
+# chmod +x start-mfu-tidloop.sh
