c69347f63521fa58c461620564bbea1c70097ec7
Osathi Jantrasri <otfor01@gmail.com>
2025-03-22 14:16:51 +0700

    feat: update docker-compose config

--- backend/notification/start-mfu-tidloop.sh
+++ backend/notification/start-mfu-tidloop.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
 # Get the IP address from the specified interface
 export IPADDRESS=$(ipconfig getifaddr en0)
-export DB_URL=jdbc:postgresql://host.docker.internal:5432/InventoryManagement
-export DB_USER=postgres
-export DB_PASSWORD=user1234
+
+# config
+export POSTGRES_DB_NAME=InventoryManagement
+export POSTGRES_PORT=5432
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=user1234
+export POSTGRES_DB_SEVER_NAME=dbserver1
+export POSTGRES_DB_TOPIC_PREFIX=dbserver1
 
 echo "mfu-tidloop Using Host IPADDRESS: $IPADDRESS"
 # Now run docker-compose with the exported IPADDRESS variable
